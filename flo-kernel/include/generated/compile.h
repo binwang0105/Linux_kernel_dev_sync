8f2851c03d82074ce7caa52b1afceb16b5b02330
W4118 Student <w4118@w4118>
2015-10-18 01:30:23 -0400

    add 4 syscalls

--- flo-kernel/include/generated/compile.h
+++ flo-kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 6 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Fri Oct 16 23:04:55 EDT 2015"
+#define UTS_VERSION "#6 PREEMPT Sat Oct 17 20:48:22 EDT 2015"
 #define LINUX_COMPILE_BY "w4118"
 #define LINUX_COMPILE_HOST "w4118"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
--- flo-kernel/kernel/light.c
+++ flo-kernel/kernel/light.c
@@ -4,15 +4,17 @@
 #include <asm-generic/errno-base.h>
 #include <asm/uaccess.h>
 
+/* open a kernel space */
 static struct light_intensity kernel_li;
 
-/* update kernel light intensity */
 SYSCALL_DEFINE1(set_light_intensity, struct light_intensity __user *, user_light_intensity){
-
+	
+	/* current_euid()==0 grant permission, otherwise deny */
 	if(current_euid() != 0){
 		return -EACCES;
 	}
-
+	
+	/* copy_from_user, &kernel_space, &user_space, sizeof */
 	if(copy_from_user(&kernel_li, user_light_intensity, sizeof(struct light_intensity))){
 		return -EFAULT;
 	}
@@ -20,19 +22,11 @@ SYSCALL_DEFINE1(set_light_intensity, struct light_intensity __user *, user_light
 	return 0;
 }
 
-/* get light intensity from kernel */
 SYSCALL_DEFINE1(get_light_intensity, struct light_intensity __user *, user_light_intensity){
-
+	/* copy_to_user, &user_space, &kernel_space, sizeof */
 	if(copy_to_user(user_light_intensity, &kernel_li, sizeof(struct light_intensity))){
 		return -EFAULT;
 	}
 
 	return 0;
 }
-
-
-
-
-
-
-
